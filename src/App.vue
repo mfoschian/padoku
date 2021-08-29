30e7709b9a1be31ca05c9702a45294bcc0807276
Marco Foschian <marco.foschian@gmail.com>
2021-08-29 16:00:07 +0200

    first import

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<div id="app">PadOku</div>
+		<div><Cell class="cell" /></div>
+	</div>
+</template>
+
+<script>
+import Cell from "./components/cell.vue"
+
+
+export default {
+	name: "App",
+	components: {
+		Cell
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+div.cell {
+	height: 100px;
+	width: 100px;
+}
+</style>
---
+++ src/components/cell.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="cell-notes" v-if="value == 0">
+		<div v-for="i in rows" :key="i" class="row">
+			<div v-for="j in cols" :key="j" class="col">
+				<div class="item">{{ i }},{{ j }}</div>
+			</div>
+		</div>
+	</div>
+	<div v-else class="cell-value">
+		<div class="value">
+			{{ value }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		value: { type: [Number,String], default: 0 },
+		notes: { type: Array, default: () => [] },
+		overrides: { type: Array, default: () => [] },
+		rows: { type: Number, default: 3 },
+		cols: { type: Number, default: 3 },
+	}
+}
+
+</script>
+
+<style scoped>
+div.cell {
+	border: 1px solid black;
+	background-color: beige;
+	display: flex;
+	flex-flow: column;
+	justify-content: space-around;
+}
+div.row {
+	/* background-color: beige; */
+	display: flex;
+	flex-flow: row;
+	justify-content: space-around;
+}
+div.col {
+	border: 1px solid black;
+}
+</style>
